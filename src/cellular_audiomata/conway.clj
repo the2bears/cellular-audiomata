378cd3521b4811f5305999c1385e1bd37b727366
William Swaney <the2bears@gmail.com>
2017-05-22 21:46:18 -0700

    Rotate and translate

--- src/cellular_audiomata/conway.clj
+++ src/cellular_audiomata/conway.clj
@@ -55,6 +55,33 @@
      (swap! pattern-registry-ref assoc name pattern)
      pattern)))
 
+(comment
+  "2nd example of each seems better. Check hiccup and play-cljs"
+  (add pattern-name :at x y :rotate 90 :around x2 y2)
+  (add (rotate pattern-name 90 :around x2 y2) :at x y)
+  (add pattern-name :at x y :flip :vertical :on y2)
+  (add (flip pattern-name :vertical :on y2) :at x y)
+  (generate-grid [[:add {:pattern pattern-name :x x :y y} 
+                        :rotate 90 :cx x2 :cy y2]
+                  [:add {:pattern pattern-name :x x :y y
+                         :flip-vertical y2}]]))
+(defn translate [[x y] dx dy]
+  [(+ x dx) (+ y dy)])
+
+(defn rotate [[x y] d cx cy]
+  (let [[x y] (translate [x y] (- cx) (- cy))
+        p (case d
+             90 [y (- x)] 
+             180 [(- x) (- y)]
+             270 [(- y) x]
+             default [x y])]
+     (translate p cx cy)))  
+
+(defn rotate-cells [cells d cx cy]
+  (let [s (seq cells)]
+    (->> (map #(rotate % d cx cy) s)
+         (set))))
+
 ; patterns
 (def blinker #{[2 1] [2 2] [2 3]})
 (def glider #{[2 0] [2 1] [2 2] [1 2] [0 1]})
