1a81351f456f064a24448f12a35dffa77823daa2
William Swaney <the2bears@gmail.com>
2017-06-30 20:04:20 -0700

    Added tests for DSL

--- test/cellular_audiomata/pattern_test.clj
+++ test/cellular_audiomata/pattern_test.clj
@@ -78,3 +78,30 @@
          (rotate blinker 180 5 5)))   
   (is (= (flip (flip "blinker" :x 0) :y 0)
          (rotate blinker 180 0 0))))   
+
+(deftest test-pattern! []
+  (is (= (create-world [[:add {:pattern blinker}]])
+         blinker))
+  (is (= (create-world [[:rotate {:pattern blinker :d 90}]])
+         (rotate blinker 90)))
+  (is (= (create-world [[:translate {:pattern blinker :dx 2 :dy 1}]])
+         (translate blinker 2 1)))
+  (is (= (create-world [[:flip {:pattern blinker :axis :x :a 4}]])
+         (flip blinker :x 4)))
+  (is (= (create-world [[:add {:pattern [[:add {:pattern blinker}]
+                                         [:add {:pattern glider}]]}]])
+         (add blinker glider)))
+  (is (= (create-world [[:add {:pattern [:translate {:pattern blinker :dx 2 :dy 0}]}]
+                        [:flip {:pattern glider :axis :x :a 5} :as "flipped"]
+                        [:rotate {:pattern glider2 :d 90} :as "rotated"]
+                        [:translate {:pattern light-spaceship :dx 10 :dy 10}]]))
+      (add (add (translate blinker 2 0)
+                (flip glider :x 5)
+                (rotate glider2 90)
+                (translate light-spaceship 10 10))))
+  (is (= (create-world [[:add {:pattern [:translate {:pattern [[:add {:pattern blinker}]
+                                                               [:add {:pattern glider2}]]
+                                                     :dx 10 :dy 10} :as "something"]}
+                         [:translate {:pattern "something" :dx -20 :dy -20}]]]))
+      (add (store-pattern "something" (add (translate (add blinker glider2) 10 10)))
+           (translate "something" -20 -20))))
