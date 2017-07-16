4c9b1c80459dcef783d8e43e8c12865939f528a9
William Swaney <the2bears@gmail.com>
2017-07-16 17:34:47 +0800

    Back to using Clojure 1.8 until Overtone uploads spec fixed version to Clojars. Or, better yet, until Overtone is separated from a dep on this project and part of an example.

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [clojure-lanterna "0.9.4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;[org.clojure/clojure "1.9.0-alpha16"]
+                 [clojure-lanterna "0.9.7"]
                  [overtone "0.10.1"]
                  [org.clojure/test.check "0.9.0"]]
   :main ^:skip-aot cellular-audiomata.core
